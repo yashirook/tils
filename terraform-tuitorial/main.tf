1b2626aac1ee685d0a5342146abadab033fd14d8
八代健太郎 <yashiro.kentaro@icloud.com>
2020-01-10 00:28:12 +0900

    update

--- terraform-tuitorial/main.tf
+++ terraform-tuitorial/main.tf
@@ -22,8 +22,42 @@ resource "google_compute_instance" "vm_instance" {
   }
 
   network_interface {
-    network = google_compute_network.vpc_network.name
+    network = google_compute_network.vpc_network.self_link
     access_config {
+      nat_ip = google_compute_address.vm_static_ip.address
     }
   }
 }
+
+resource "google_compute_address" "vm_static_ip" {
+  name = "terraform-static-ip"
+}
+
+/* resource "google_storage_bucket" "example_bucket" {
+  name = "terraform-example-bucket-yashiroken-20200109"
+  location = "US"
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page = "404.html"
+  }
+}
+
+resource "google_compute_instance" "another_instance" {
+  depends_on = [google_storage_bucket.example_bucket]
+
+  name = "terraform-instance-2"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+} */
